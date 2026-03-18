71cc83ba42777033105673efbfdaa91e963656a4
Jaret <jaretthompson11@gmail.com>
2026-03-18 16:49:54 +0200

    Initial reload fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  server: {
-    historyApiFallback: true
-  }
-})
+	plugins: [tailwindcss()],
+	server: {
+		historyApiFallback: true,
+	},
+	preview: {
+		historyApiFallback: true,
+	},
+});
